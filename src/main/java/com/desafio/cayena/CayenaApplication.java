43a5da7952a223b0f7ea017cb9f1c26222e59d1b
Sabryna Araujo <sabrynaaraujodev@gmail.com>
2023-08-02 23:56:27 -0300

    iniciando estrutura do projeto

---
+++ src/main/java/com/desafio/cayena/CayenaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.cayena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CayenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CayenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/cayena/CayenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.cayena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CayenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
