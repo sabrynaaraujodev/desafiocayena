383fa626ed9e24e69f65a2c6caeff39b075d701e
Sabryna Araujo <sabrynaaraujodev@gmail.com>
2023-08-03 19:49:10 -0300

    inserindo api de get by id e alterar produtos

--- src/main/java/com/desafio/cayena/controller/ProdutosController.java
+++ src/main/java/com/desafio/cayena/controller/ProdutosController.java
@@ -3,14 +3,13 @@ package com.desafio.cayena.controller;
 import com.desafio.cayena.model.Produtos;
 import com.desafio.cayena.repository.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/produtos/")
 public class ProdutosController {
 
     @Autowired
@@ -25,5 +24,20 @@ public class ProdutosController {
     public void cadastroProdutos(@RequestBody Produtos produtos) {
         produtosRepository.save(produtos);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produtos> getById(@PathVariable Integer id) {
+        return produtosRepository.findById(id)
+                .map (ResponseEntity::ok)
+                .orElse (ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<Produtos> alteraProdutos( @RequestBody Produtos produtos){
+        return produtosRepository.findById(produtos.getId())
+                .map(resposta -> ResponseEntity.ok().body(produtosRepository.save(produtos)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 
 }
